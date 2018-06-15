54ce782e475eb33dfcf9be079f09c91b800c8d15
johnny6464 <johnny19641964@gmail.com>
2018-06-15 14:09:50 +0800

    first commit

--- app.py
+++ app.py
@@ -73,14 +73,13 @@ def handle_message(event):
     index = random.randint(0, len(images) - 1)
     url = images[index].link
     if event.message.text == "corgi" or event.message.text == "柯基":
-        print("a")
         message = ImageSendMessage(original_content_url=url, preview_image_url=url)
     elif event.message.text == "news":
         content = technews()
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
     else:
         message = TextSendMessage(text=event.message.text)
-        line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(event.reply_token, message)
 
 
 import os
